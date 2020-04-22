6d3504abf6c09c0c0d7229a555dc4d09bbabe393
isabel k. lee <hello@isabelklee.co>
2020-04-22 12:48:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => renderBooks(json))
+  //.then(json => fifthBook(json))
 }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
+  let number = 1
+
   json.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
+    h2.innerText = `${number++}. ${book.name}`
     main.appendChild(h2)
   })
+
 }
+
+// this function identifies the fifth book and returns the title
+
+// function fifthBook(json) {
+//   const main = document.querySelector('main')
+//   const h2 = document.createElement('h2')
+//   let bookName = json[4]["name"]
+//   h2.innerText = `${bookName}`
+//   main.appendChild(h2)
+// }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
